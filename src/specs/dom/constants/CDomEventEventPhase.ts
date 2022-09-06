0683cb4245128c36b574383d0bcf8a7c004d047b
viell <viell.dev@gmail.com>
2022-09-06 11:02:30 +0200

    Updated DomEvent and DomEventInit files

--- src/specs/dom/constants/CDomEventEventPhase.ts
+++ src/specs/dom/constants/CDomEventEventPhase.ts
@@ -1,6 +1,10 @@
-export enum DomEventPhase {
-  NONE = 0,
-  CAPTURING_PHASE = 1,
-  AT_TARGET = 2,
-  BUBBLING_PHASE = 3,
-}
+import type { Values } from "@/helpers/Values.js";
+
+export const CDomEventEventPhase = {
+  NONE: 0,
+  CAPTURING_PHASE: 1,
+  AT_TARGET: 2,
+  BUBBLING_PHASE: 3,
+} as const;
+
+export type CDomEventEventPhase = Values<typeof CDomEventEventPhase>;
