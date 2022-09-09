c08c86104b744f7ed2845c688d3b2ca5c2080979
viell <viell.dev@gmail.com>
2022-09-09 12:36:06 +0200

    Updated DocumentFragment file

--- src/specs/dom/interfaces/IDomDocumentFragment.ts
+++ src/specs/dom/interfaces/IDomDocumentFragment.ts
@@ -1,6 +1,12 @@
+import type { IWrapperConstructors } from "@/globals/IWrapper.js";
 import type { MDomNonElementParentNode } from "../mixins/MDomNonElementParentNode.js";
 import type { MDomParentNode } from "../mixins/MDomParentNode.js";
 import type { IDomNode } from "./IDomNode.js";
+
+export interface IDomDocumentFragmentConstructor
+  extends IWrapperConstructors<DocumentFragment> {
+  new (): IDomDocumentFragment<DocumentFragment>;
+}
 
 export interface IDomDocumentFragment<N extends DocumentFragment>
   extends IDomNode<N>,
