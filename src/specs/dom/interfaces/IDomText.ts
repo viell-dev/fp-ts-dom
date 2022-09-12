63934f677ae9292fe0ff5e3a4c9f2ec078e43856
viell <viell.dev@gmail.com>
2022-09-12 08:47:07 +0200

    Updated Text, CDATASection, ProcessingInstruction and Comment files

--- src/specs/dom/interfaces/IDomText.ts
+++ src/specs/dom/interfaces/IDomText.ts
@@ -1,9 +1,15 @@
+import type { IWrapperConstructors } from "@/globals/IWrapper.js";
+import type * as E from "fp-ts/Either";
 import type { MDomSlottable } from "../mixins/MDomSlottable.js";
 import type { IDomCharacterData } from "./IDomCharacterData.js";
+
+export interface IDomTextConstructors extends IWrapperConstructors<Text> {
+  new (data?: string): IDomText<Text>;
+}
 
 export interface IDomText<N extends Text>
   extends IDomCharacterData<N>,
     MDomSlottable {
-  splitText(offset: number): IDomText<Text>;
+  splitText(offset: number): E.Either<RangeError, IDomText<Text>>;
   readonly wholeText: string;
 }
