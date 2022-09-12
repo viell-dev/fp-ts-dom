de963bce40948da2044c44fee78dc6b5a0463496
viell <viell.dev@gmail.com>
2022-09-12 15:53:21 +0200

    Changed handling of internals to avoid type assertions

--- src/impls/geometry/GeometryDOMQuad.ts
+++ src/impls/geometry/GeometryDOMQuad.ts
@@ -7,6 +7,7 @@ import type {
   IGeometryDOMQuad,
   IGeometryDOMQuadConstructors,
 } from "@/specs/geometry/interfaces/IGeometryDOMQuad.js";
+import { pipe } from "fp-ts/function";
 import * as O from "fp-ts/Option";
 import { GeometryDOMPoint } from "./GeometryDOMPoint.js";
 import { GeometryDOMRect } from "./GeometryDOMRect.js";
@@ -46,51 +47,47 @@ export class GeometryDOMQuad
 
   protected p1Internal: O.Option<GeometryDOMPoint> = O.none;
   get p1(): GeometryDOMPoint {
-    if (O.isNone(this.p1Internal)) {
-      /* eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-      -- `O.some` always returns an `O.Some`. */
-      this.p1Internal = O.some(
-        new GeometryDOMPoint(this.native.p1)
-      ) as O.Some<GeometryDOMPoint>;
-    }
-
-    return this.p1Internal.value;
+    return pipe(
+      this.p1Internal,
+      O.getOrElse(() => {
+        const point = new GeometryDOMPoint(this.native.p1);
+        this.p1Internal = O.some(point);
+        return point;
+      })
+    );
   }
   protected p2Internal: O.Option<GeometryDOMPoint> = O.none;
   get p2(): GeometryDOMPoint {
-    if (O.isNone(this.p2Internal)) {
-      /* eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-      -- `O.some` always returns an `O.Some`. */
-      this.p2Internal = O.some(
-        new GeometryDOMPoint(this.native.p2)
-      ) as O.Some<GeometryDOMPoint>;
-    }
-
-    return this.p2Internal.value;
+    return pipe(
+      this.p2Internal,
+      O.getOrElse(() => {
+        const point = new GeometryDOMPoint(this.native.p2);
+        this.p2Internal = O.some(point);
+        return point;
+      })
+    );
   }
   protected p3Internal: O.Option<GeometryDOMPoint> = O.none;
   get p3(): GeometryDOMPoint {
-    if (O.isNone(this.p3Internal)) {
-      /* eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-      -- `O.some` always returns an `O.Some`. */
-      this.p3Internal = O.some(
-        new GeometryDOMPoint(this.native.p3)
-      ) as O.Some<GeometryDOMPoint>;
-    }
-
-    return this.p3Internal.value;
+    return pipe(
+      this.p3Internal,
+      O.getOrElse(() => {
+        const point = new GeometryDOMPoint(this.native.p3);
+        this.p3Internal = O.some(point);
+        return point;
+      })
+    );
   }
   protected p4Internal: O.Option<GeometryDOMPoint> = O.none;
   get p4(): GeometryDOMPoint {
-    if (O.isNone(this.p4Internal)) {
-      /* eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-      -- `O.some` always returns an `O.Some`. */
-      this.p4Internal = O.some(
-        new GeometryDOMPoint(this.native.p4)
-      ) as O.Some<GeometryDOMPoint>;
-    }
-
-    return this.p4Internal.value;
+    return pipe(
+      this.p4Internal,
+      O.getOrElse(() => {
+        const point = new GeometryDOMPoint(this.native.p4);
+        this.p4Internal = O.some(point);
+        return point;
+      })
+    );
   }
   getBounds(): GeometryDOMRect {
     return new GeometryDOMRect(this.native.getBounds());
