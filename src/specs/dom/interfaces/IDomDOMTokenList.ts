80ff43b8c2bf38b1c9672dba7b8ad4f1a08aa487
viell <viell.dev@gmail.com>
2022-09-12 10:14:15 +0200

    Updated DOMTokenList file

--- src/specs/dom/interfaces/IDomDOMTokenList.ts
+++ src/specs/dom/interfaces/IDomDOMTokenList.ts
@@ -1,14 +1,44 @@
+import type {
+  InvalidCharacterErrorDomException,
+  SyntaxErrorDomException,
+} from "@/exceptions/DomException.js";
 import type { IWrapper } from "@/globals/IWrapper.js";
+import type * as E from "fp-ts/Either";
 import type * as O from "fp-ts/Option";
 
-export interface IDomDOMTokenList<N extends DOMTokenList> extends IWrapper<N> {
+export interface IDomDOMTokenList<N extends DOMTokenList>
+  extends IWrapper<N>,
+    Iterable<string> {
   readonly length: number;
   item(index: number): O.Option<string>;
+  //[index: number]: string;
   contains(token: string): boolean;
-  add(...tokens: string[]): void;
-  remove(...tokens: string[]): void;
-  toggle(token: string, force?: boolean): boolean;
-  replace(token: string, newToken: string): boolean;
-  supports(token: string): boolean;
+  add(
+    ...tokens: string[]
+  ): E.Either<
+    SyntaxErrorDomException | InvalidCharacterErrorDomException,
+    void
+  >;
+  remove(
+    ...tokens: string[]
+  ): E.Either<
+    SyntaxErrorDomException | InvalidCharacterErrorDomException,
+    void
+  >;
+  toggle(
+    token: string,
+    force?: boolean
+  ): E.Either<
+    SyntaxErrorDomException | InvalidCharacterErrorDomException,
+    boolean
+  >;
+  replace(
+    token: string,
+    newToken: string
+  ): E.Either<
+    SyntaxErrorDomException | InvalidCharacterErrorDomException,
+    boolean
+  >;
+  supports(token: string): E.Either<TypeError, boolean>;
   value: string;
 }
