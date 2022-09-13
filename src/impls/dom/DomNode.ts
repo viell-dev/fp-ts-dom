d2510aeaca0d4436b6cf5e0e6db0d08b6d1facb1
viell <viell.dev@gmail.com>
2022-09-13 14:09:00 +0200

    Removed non-existant constructors interface import

--- src/impls/dom/DomNode.ts
+++ src/impls/dom/DomNode.ts
@@ -2,9 +2,8 @@ import { StaticImplements } from "@/decorators/StaticImplements.js";
 import type {
   IDomNode,
   IDomNodeConstants,
-  IDomNodeConstructors,
 } from "@/specs/dom/interfaces/IDomNode.js";
 import { DomNodeBase } from "./DomNodeBase.js";
 
-@StaticImplements<IDomNodeConstructors & IDomNodeConstants>()
+@StaticImplements<IDomNodeConstants>()
 export class DomNode extends DomNodeBase<Node> implements IDomNode<Node> {}
